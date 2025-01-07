2cd3ae1cb0615f88362522ed95a22710deaaf8ff
Abdelrahman <abdelrahmanhesham477@gmail.com>
2025-01-07 18:48:00 +0200

    add layout and components

---
+++ app/components/BaseNavigation.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="py-4 border-b">
+    <nav class="flex container items-center">
+      <NuxtLink to="/" class="flex gap-1 items-center">
+        <NuxtImg width="45px" src="/images/logo.png" format="webp" alt="" />
+        <span class="text-3xl font-extrabold">Foodie</span>
+      </NuxtLink>
+      <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
