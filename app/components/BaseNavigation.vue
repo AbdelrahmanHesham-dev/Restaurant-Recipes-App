15927490ff045a8b4ec1775130b9016ca1d881d1
Abdelrahman <abdelrahmanhesham477@gmail.com>
2025-01-07 19:02:05 +0200

    fix nav responsive

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -1,18 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isMenuOpen = ref(false);
+</script>
 
 <template>
   <header class="py-4 border-b">
     <nav class="flex container items-center">
       <NuxtLink to="/" class="flex gap-1 items-center">
         <NuxtImg width="45px" src="/images/logo.png" format="webp" alt="" />
-        <span class="text-3xl font-extrabold">Foodie</span>
+        <span class="text-3xl font-[900]">Foodie</span>
       </NuxtLink>
-      <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+
+      <button
+        class="ml-auto md:hidden text-3xl"
+        @click="isMenuOpen = !isMenuOpen"
+        aria-label="Toggle Menu"
+      >
+        ☰
+      </button>
+
+      <ul
+        :class="[
+          'flex gap-6 text-xl font-bold capitalize',
+          isMenuOpen
+            ? 'flex flex-col absolute top-16 right-0 bg-white w-full p-4 shadow-lg'
+            : 'hidden',
+          'md:flex md:static md:flex-row md:ml-auto md:shadow-none md:bg-transparent',
+        ]"
+      >
         <li>
-          <NuxtLink to="/">Home</NuxtLink>
+          <NuxtLink to="/" class="block py-2 md:py-0">Home</NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/about">About</NuxtLink>
+          <NuxtLink to="/recipes" class="block py-2 md:py-0">Recipes</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about" class="block py-2 md:py-0">About</NuxtLink>
         </li>
       </ul>
     </nav>
